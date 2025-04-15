a826cb0f5a004de6cf8b27e6e5f243a6bb3f6e71
ducminhscode <ducmin76@gmail.com>
2025-04-15 16:04:27 +0700

    edit volume

--- Django/backendApi/openstackapi/urls.py
+++ Django/backendApi/openstackapi/urls.py
@@ -1,17 +1,14 @@
 from django.urls import path
-from .views import LoginKeystone, InstancesAPIView, ImageAPIView, NetworkAPIView, VolumeAPIView
+from .views import LoginKeystone, InstancesAPIView, NetworkAPIView, VolumeAPIView
 
 urlpatterns = [
     path("login/", LoginKeystone.as_view()),
     path("instances/", InstancesAPIView.as_view()),
     path('instances/<str:id>/', InstancesAPIView.as_view(), name='instance_detail'),
 
-    path('images/', ImageAPIView.as_view(), name='image-list'),
-    path('images/<str:image_id>/', ImageAPIView.as_view(), name='image-detail'),
-
     path('networks/', NetworkAPIView.as_view()),
     path('networks/<str:network_id>/', NetworkAPIView.as_view()),
 
-    path('volumes/', VolumeAPIView.as_view()),  # GET all, POST
-    path('volumes/<str:volume_id>/', VolumeAPIView.as_view()),  # GET one, DELETE
+    path('volumes/', VolumeAPIView.as_view()),
+    path('volumes/<str:volume_id>/', VolumeAPIView.as_view()),
 ]
