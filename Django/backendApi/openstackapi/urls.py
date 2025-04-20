0e4989d8d6bc9ba7e7396bb189dd3292416772cd
ducminhscode <ducmin76@gmail.com>
2025-04-20 15:20:20 +0700

    routers p3 + security

--- Django/backendApi/openstackapi/urls.py
+++ Django/backendApi/openstackapi/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import LoginKeystone, InstancesAPIView, NetworkAPIView, VolumeAPIView, SubnetAPIView, PortAPIView, \
     FlavorAPIView, RegisterAPIView, ImagesAPIView, SecurityGroupAPIView, KeyPairAPIView, \
     RestoreInstanceFromSnapshotAPIView, RestoreVolumeFromSnapshotAPIView, VolumeSnapshotAPIView, \
-    InstanceSnapshotAPIView, RouterAPIView
+    InstanceSnapshotAPIView, RouterAPIView, RouterInterfaceAPIView
 
 urlpatterns = [
     path("login/", LoginKeystone.as_view()),
@@ -18,6 +18,8 @@ urlpatterns = [
     path('networks/<str:network_id>/', NetworkAPIView.as_view()),
 
     path('security-groups/', SecurityGroupAPIView.as_view()),
+    path('networks/<str:security_group_id>/security-groups/', SecurityGroupAPIView.as_view()),
+
     path('key-pairs/', KeyPairAPIView.as_view()),
     path('key-pairs/<str:key_name>/', KeyPairAPIView.as_view()),
 
@@ -26,6 +28,8 @@ urlpatterns = [
     path('networks/<str:network_id>/ports/', PortAPIView.as_view()),
     path('routers/', RouterAPIView.as_view()),
     path('networks/<str:router_id>/routers', RouterAPIView.as_view()),
+    path('networks/<str:router_id>/add_router_interface', RouterInterfaceAPIView.as_view(), {'action': 'add_router_interface'}),
+    path('networks/<str:router_id>/remove_router_interface', RouterInterfaceAPIView.as_view(), {'action': 'remove_router_interface'}),
 
     path('volumes/', VolumeAPIView.as_view()),
     path('volumes/<str:volume_id>/', VolumeAPIView.as_view()),
