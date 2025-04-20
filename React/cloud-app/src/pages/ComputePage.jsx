f035ffab086446e763ac8fc89a324866e62cfcc1
Cyter <tu2004latoi@gmail.com>
2025-04-20 21:41:03 +0700

    tudualen

--- React/cloud-app/src/pages/ComputePage.jsx
+++ React/cloud-app/src/pages/ComputePage.jsx
@@ -150,6 +150,8 @@ export default function ComputePage() {
     }
   };
 
+  
+
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-3xl font-bold text-center mb-8">Quản lý Instances</h1>
